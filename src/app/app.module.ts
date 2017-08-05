4ce4137930b3c3247fccec1e39876f24cbcb338a
Diptangshu Chakrabarty <zubin@Diptangshus-Mac-mini.local>
2017-08-05 11:50:46 +0530

    Arranged HTML into separate components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { QuizProgressComponent } from './quiz-progress/quiz-progress.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    FooterComponent,
+    QuizProgressComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quiz-progress/quiz-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-progress',
+  templateUrl: './quiz-progress.component.html',
+  styleUrls: ['./quiz-progress.component.css']
+})
+export class QuizProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
